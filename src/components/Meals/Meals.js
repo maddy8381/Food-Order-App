c09b0dc6ae6da06e705f82598cea2c6f83df9386
unknown <sgmadankar@gmail.com>
2021-09-12 17:13:12 +0530

    Meal list component added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MealsSummary from './MealsSummary';
+import MealsAvailable from './MealsAvailable';
+
+const Meals = () => {
+
+    return (
+        <React.Fragment>
+            <MealsSummary />
+            <MealsAvailable />
+        </React.Fragment>
+    );
+
+}
+
+export default Meals;
