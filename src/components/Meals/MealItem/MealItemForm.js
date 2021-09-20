7fff8c300fdefb7f7b42b4fe317b77208bde4f87
unknown <sgmadankar@gmail.com>
2021-09-20 09:02:16 +0530

    Using ForwardRef to get data from Custom Input Component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,31 @@
 import classes from './MealItemForm.module.css';
 import Input from './../../UI/Input';
+import { useRef } from 'react';
 
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
 
-    return <form className={classes.form}>
-        <Input label="Amount" input={{   //passing props
-            id: 'amount_' + props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount; //convert string to no
+
+        if (enteredAmount.trim().length === 0 || enteredAmount < 1 || enteredAmount > 5)
+            return;
+    }
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInputRef}
+            label="Amount" input={{   //passing props
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
         <button>+ Add</button>
     </form>
 }
